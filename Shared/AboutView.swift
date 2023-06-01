94282976df5e3f5519e40e8dd105ff7d14e98e08
Liu Ziyi <ziyi.liu02@gmail.com>
2023-06-01 21:18:14 +0800

    - Add images for AboutView to Assets - Build the AboutView

--- Shared/AboutView.swift
+++ Shared/AboutView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView(showsIndicators:false) {
+            
+            VStack(alignment:.leading) {
+                Text("About")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image("restaurant-inside")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom)
+                
+                
+                Text("Sukiyabashi Jiro (すきやばし次郎, Sukiyabashi Jirō) is a sushi restaurant in Ginza, Chūō, Tokyo, Japan. It is owned and operated by sushi master Jiro Ono.[1] It was the first sushi restaurant in the world[2] to receive three stars from the Michelin Guide,[3] although it was removed from the Michelin Guide in November 2019 as it does not receive reservations from the general public,[4][5] instead requiring reservations to be made through the concierge of a luxury hotel.")
+                
+                Image("map")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom)
+                
+                Text("""
+    Tsukamoto Sogyo Building
+    Basement Floor 1
+    2-15, Ginza 4-chome
+    Chūō, Tokyo
+    """)
+                
+            }
+            .padding(.horizontal)
+            
+        }
+        
     }
 }
 
